fb3cfd055537d1be45bff5d3187ea4f376e84aa2
Niels-Åge Schultz <niel862b@elevcampus.dk>
2020-05-18 16:54:05 +0200

    alt virker bare helt perfekt wink wink

--- eShopWeb/Pages/Index.cshtml.cs
+++ eShopWeb/Pages/Index.cshtml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DataLayer.Entities;
+using eShopWeb.Extensions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -15,11 +17,13 @@ namespace eShopWeb.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        [BindProperty(SupportsGet = true)]
         public IEnumerable<Produkt> Produkter { get; set; }
+        [BindProperty(SupportsGet = true)]
         public IEnumerable<Kategori> Kategorier { get; set; }
+        [BindProperty(SupportsGet = true)]
         public IEnumerable<Producent> Producenter { get; set; }
-
+        public Produkt Produkt { get; set; }
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
 
@@ -81,6 +85,70 @@ namespace eShopWeb.Pages
             catch (Exception ex)
             {
                 Log.Error(ex, "Det virker squ ik(OnPostProducent)");
+            }
+
+            Log.CloseAndFlush();
+        }
+        public void OnPostKategori(string kategoriNavn)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs\\myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Loading index");
+
+            try
+            {
+                Produkter = _eShopService.GetProdukterByName(kategoriNavn).ToList();
+                Kategorier = _eShopService.GetKategorier().ToList();
+                Producenter = _eShopService.GetProducenter().ToList();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Det virker squ ik(OnPostKategori)");
+            }
+
+            Log.CloseAndFlush();
+        }
+        public void OnPostAddToCart(int produktId)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs\\myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Loading index");
+
+            if (ModelState.IsValid)
+            {
+                Produkt = _eShopService.GetProduktById(produktId);
+                List<Produkt> kurv = HttpContext.Session.Get<List<Produkt>>("kurv");
+                if (kurv == null)
+                {
+                    kurv = new List<Produkt>();
+                }
+                //kurv.Add(Produkt);
+                kurv.Add(new Produkt
+                {
+                    ProduktId = Produkt.ProduktId,
+                    ProduktNavn = Produkt.ProduktNavn,
+                    Pris = Produkt.Pris
+                });
+                HttpContext.Session.Set("kurv", kurv);
+
+            }
+            try
+            {
+                Produkter = _eShopService.GetProdukterByName().ToList();
+                Kategorier = _eShopService.GetKategorier().ToList();
+                Producenter = _eShopService.GetProducenter().ToList();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Det virker squ ik(OnPostKategori)");
             }
 
             Log.CloseAndFlush();
